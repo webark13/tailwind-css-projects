f721fe6a9d403366baf7f09729762c55f25a7709
webark13 <rehmank134@gmail.com>
2022-07-19 23:51:54 +0530

    contact-form

---
+++ tailwind-contact-form/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/**/index.html',
+    './layouts/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
